869f93e56039ce815ca6978924163f6ebede31c3
Oh Daehyeon <xzdheogus1@gmail.com>
2022-12-18 21:07:11 +0900

    v1.0.2

--- server.js
+++ server.js
@@ -22,3 +22,7 @@ app.get('/beauty', function(request, response){
 app.get('/', function(request, response){
     response.sendFile(__dirname + '/index.html');
 });
+
+app.get('/write', function(request, response){
+    response.sendFile(__dirname + '/write.html');
+});
